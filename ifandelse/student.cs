04a8ecd2c90ed84538631b904578725b714651f5
KHALIG77 <118839593+KHALIG77@users.noreply.github.com>
2022-12-07 05:25:40 -0800

    If and else for Do

---
+++ ifandelse/student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+int number;
+do
+{
+
+    Console.WriteLine("Zehmet olmasa  musbet eded daxil edin");
+    number=int.Parse(Console.ReadLine());
+} while (number<0);
+
+if (number<=100) {
+    if (number>=91&&number<=100)
+    {
+
+        Console.WriteLine("A");
+    }
+   else if (number>=81&&number<=90)
+    {
+
+        Console.WriteLine("B");
+    }
+    else if (number>=71&&number<=80)
+    {
+
+        Console.WriteLine("C");
+
+    }
+    else if (number>=61&&number<=70)
+    {
+
+        Console.WriteLine("D");
+    }
+    else if (number>=51&&number<=60)
+    {
+
+        Console.WriteLine("E");
+    }
+    else {
+
+        Console.WriteLine("Kesildiniz");
+    }
+    
+
+
+}
+else
+{
+    Console.WriteLine("Zehmet olmasa 0-100 intervalinda eded daxil edin");
+}
+
+
---
+++ ifandelse/treeandseven.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+int num;
+do
+{
+
+    Console.WriteLine("Zehmet olmasa  musbet eded daxil edin");
+    num=int.Parse(Console.ReadLine());
+} while (num<0);
+if (num%21==0)
+{
+    Console.WriteLine("Daxil olunan eded 3 ve 7 -ye bolunur");
+}
+else
+{
+    Console.WriteLine("Daxil olunan eded 3 ve 7 -ye bolunmur");
+}
+
+
