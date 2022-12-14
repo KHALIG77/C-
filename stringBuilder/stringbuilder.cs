9b3a2be9e77f6ffb88e4c3da3bbf497f2f525e87
KHALIG77 <118839593+KHALIG77@users.noreply.github.com>
2022-12-14 05:28:39 -0800

    String

---
+++ stringBuilder/stringbuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] convertInput = input.Split(' ');
+
+
+            StringBuilder strBuilder = new StringBuilder();
+
+            for (int i = 0; i < convertInput.Length; i++)
+            {
+                for (int j = convertInput[i].Length - 1; j >= 0; j--)
+                {
+                    strBuilder.Append(convertInput[i][j]);
+                }
+                if (i != convertInput.Length - 1)
+                {
+                    strBuilder.Append(" ");
+                }
+            }
+          
+
+            Console.WriteLine(strBuilder);
+
+
+
+
+
+
+
+
+        }
+    }
+}
+//int[] arr = {1,6,2};
+//int[] arr2 = arr;
+//arr2[0]=9;
+//Console.WriteLine(arr[0]);
+//Console.WriteLine(arr2[0]);
