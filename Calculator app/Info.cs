7c7033574670a2c2a6fee97425295e87fb1a9eb3
KHALIG77 <118839593+KHALIG77@users.noreply.github.com>
2022-12-25 19:58:12 +0400

    Calc

---
+++ Calculator app/Info.cs
@@ -0,0 +1,17 @@
+﻿
+     class Info
+    {
+       
+            virtual protected void Information()
+            {
+                Console.WriteLine("Toplamaq ucun 1 basin");
+                Console.WriteLine("Cixmaq ucun 2 basin");
+                Console.WriteLine("Vurmaq ucun 3 basin");
+                Console.WriteLine("Bolmek ucun 4 basin");
+
+
+
+            }
+        }
+
+
---
+++ Calculator app/Prop.cs
@@ -0,0 +1,9 @@
+﻿
+   abstract class Prop:Info
+    {
+        abstract public int Number1 { get; set; }
+        abstract public int Number2 { get; set; }
+        abstract public int Result { get; set; }
+    }
+
+
