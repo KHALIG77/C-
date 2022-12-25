979264b6443c4e35d80622150b4fdf21a6c0c42d
KHALIG77 <118839593+KHALIG77@users.noreply.github.com>
2022-12-25 19:57:22 +0400

    Delete Prop.cs

--- Prop.cs
+++
@@ -1,9 +0,0 @@
-﻿
-   abstract class Prop:Info
-    {
-        abstract public int Number1 { get; set; }
-        abstract public int Number2 { get; set; }
-        abstract public int Result { get; set; }
-    }
-
-
